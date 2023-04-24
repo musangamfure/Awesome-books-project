d8a771ef1414205259899d453ca5da08f7b64f00
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-04-24 16:28:05 +0100

    Add linters

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const bookCollection = localStorage.getItem("bookCollection")
-  ? JSON.parse(localStorage.getItem("bookCollection"))
+const bookCollection = localStorage.getItem('bookCollection')
+  ? JSON.parse(localStorage.getItem('bookCollection'))
   : [];
-const title = document.querySelector("#title-input");
-const author = document.querySelector("#author-input");
-const bookContainer = document.querySelector(".books-container");
+const title = document.querySelector('#title-input');
+const author = document.querySelector('#author-input');
+const bookContainer = document.querySelector('.books-container');
 
 function displayBooks() {
-  bookContainer.innerHTML = "";
+  bookContainer.innerHTML = '';
   bookCollection.forEach((book) => {
     bookContainer.insertAdjacentHTML(
-      "beforeend",
+      'beforeend',
       `<div>
             <div class ="displayTitle">
    <p>${book.title}</p>
@@ -17,30 +17,30 @@ function displayBooks() {
    </div>
     <button class="remove" type="button" id="${book.title}-${book.author}">Remove</button>
     <hr>
-    </div>`
+    </div>`,
     );
   });
 
-  const addButton = document.querySelector(".add-button");
-  addButton.addEventListener("click", () => {
+  const addButton = document.querySelector('.add-button');
+  addButton.addEventListener('click', () => {
     if (title.value && author.value) {
       const bookObject = {
         title: title.value,
         author: author.value,
       };
       bookCollection.push(bookObject);
-      title.value = "";
-      author.value = "";
+      title.value = '';
+      author.value = '';
       displayBooks();
-      localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
+      localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
     }
   });
-  const removeBtns = document.querySelectorAll(".remove");
+  const removeBtns = document.querySelectorAll('.remove');
   removeBtns.forEach((removeBtn, index) => {
-    removeBtn.addEventListener("click", () => {
+    removeBtn.addEventListener('click', () => {
       bookCollection.splice(index, 1);
       displayBooks();
-      localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
+      localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
     });
   });
 }
